dca8c550813737657400537aae6eb6f66b337d35
daga1288 <galarraga1288@hotmail.com>
2021-02-10 08:14:59 -0300

    agregamos bs

---
+++ bowlstreet/main.js
@@ -0,0 +1,5 @@
+document.querySelector('.menu-btn').addEventListener('click', ()=>{document.querySelector('.nav-menu').classList.toggle('show');
+});
+
+ScrollReveal().reveal('.showcase');
+ScrollReveal().reveal('.PROMOCIONES', {delay: 500 });
